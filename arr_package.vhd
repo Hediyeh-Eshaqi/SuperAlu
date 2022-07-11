578b1b756f60a37c3b4cb74cc6e031bb5a7b0142
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-07-11 13:09:05 +0430

    arr_package added

---
+++ arr_package.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+use ieee.numeric_std.all;
+
+package arr_package is
+
+	type input_arr is array(0 to 10) of std_logic_vector(47 downto 0); 
+	type output_arr is array(0 to 10) of std_logic_vector(7 downto 0); 
+  
+end package arr_package;
+ 
+-- Package Body Section
+package body arr_package is
+ 
+
+ 
+end package body arr_package;
